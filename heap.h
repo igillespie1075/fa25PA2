29298d64449eb166d4fbaa4587716032eeaf9d24
igllespie1075 <igillespie1075@sdsu.edu>
2025-10-31 20:54:51 -0700

    Upheap

--- heap.h
+++ heap.h
@@ -38,10 +38,25 @@ struct MinHeap {
     }
 
     void upheap(int pos, int weightArr[]) {
+        while (pos > 0) {
+            int parent = (pos -1) /2;
+            if (weightArr[data[pos]] < weightArr[data[parent]]){
+                swap (data[pos], data[parent]);
+                pos = parent;
+            }
+            else break;
+        }
         // TODO: swap child upward while smaller than parent
     }
 
     void downheap(int pos, int weightArr[]) {
+        while (true) {
+            int left = 2 * pos + 1;
+            int right = 2 * pos + 2;
+            int smallest = pos;
+
+            if ()
+        }
         // TODO: swap parent downward while larger than any child
     }
 };
